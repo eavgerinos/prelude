d8ae35d1d3a877eeca91414417107961db499254
Nikos Fertakis <nikos.fertakis@gmail.com>
2015-01-02 22:20:46 +0200

    Add fci-mode for 80 char ruler

---
+++ personal/personal-clojure.el
@@ -0,0 +1,7 @@
+;;; personal-clojure.el --- Personal settings for Clojure files.
+
+;;; Code:
+
+(add-hook 'clojure-mode-hook 'fci-mode)
+
+;;; personal-clojure.el ends here
---
+++ personal/personal-modules.el
@@ -0,0 +1,8 @@
+;;; personal-modules.el --- Extra required packages
+
+;;; Code:
+
+;; Fill column indicator (for 80char ruler)
+(prelude-require-packages '(fill-column-indicator))
+
+;;; personal-modules.el ends here
