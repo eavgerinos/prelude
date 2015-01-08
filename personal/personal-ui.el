8dab0dc920bedc089e796f29873dcf7fd901d1cc
Nikos Fertakis <nikos.fertakis@gmail.com>
2015-01-08 16:22:15 +0200

    Disable scroll bars

--- personal/personal-ui.el
+++ personal/personal-ui.el
@@ -1,3 +1,8 @@
 (global-linum-mode t)
 ;;(set-frame-font "Source Code Pro-14" nil t)
 (setq default-frame-alist '((font . "Source Code Pro-14")))
+
+;; disable scroll bar
+(scroll-bar-mode -1)
+
+;;; personal-ui.el ends here.
