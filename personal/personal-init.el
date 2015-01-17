0f428896eb898e5aef139c4331fccafdd298662c
Nikos Fertakis <nikos.fertakis@gmail.com>
2015-01-17 12:04:17 +0200

    Switch M with S for MBP

---
+++ personal/personal-init.el
@@ -0,0 +1,9 @@
+;;; personal-init.el --- Custom emacs configuration
+
+;;; Code:
+;; Swap super with meta (useful on  OS X)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
+
+(provide 'personal-init)
+;;; personal-init.el ends here
